f24444ed4f6273efc7bbedede0015d11a0406738
José Eduardo Morini Castro <Jemorini13@gmail.com>
2021-09-06 22:47:06 -0300

    deploy corrections

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,41 +5,49 @@ const puppeteer = require("puppeteer");
 
 let api = [];
 
-setInterval(async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto("https://br.investing.com/equities/");
-  const data = await page.$$eval("table tr td", (tds) =>
-    tds.map((td) => {
-      return td.innerText;
-    })
-  );
+try {
+  setInterval(async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto("https://br.investing.com/equities/");
+    const data = await page.$$eval("table tr td", (tds) =>
+      tds.map((td) => {
+        return td.innerText;
+      })
+    );
 
-  await browser.close();
+    await browser.close();
 
-  for (
-    let i = 1;
-    i < data.length;
-    i = !!data[i + 10] ? i + 10 : (i = data.length)
-  ) {
-    const obj = {};
-    obj.name = data[i];
-    obj.last = data[i + 1];
-    obj.max = data[i + 2];
-    obj.min = data[i + 3];
-    obj.variationPrice = data[i + 4];
-    obj.variationPercentage = data[i + 5];
-    obj.vol = data[i + 6];
-    obj.time = data[i + 7];
-    i + 2;
-    api.push(obj);
-  }
+    for (
+      let i = 1;
+      i < data.length;
+      i = !!data[i + 10] ? i + 10 : (i = data.length)
+    ) {
+      const obj = {};
+      obj.name = data[i];
+      obj.last = data[i + 1];
+      obj.max = data[i + 2];
+      obj.min = data[i + 3];
+      obj.variationPrice = data[i + 4];
+      obj.variationPercentage = data[i + 5];
+      obj.vol = data[i + 6];
+      obj.time = data[i + 7];
+      i + 2;
+      api.push(obj);
+    }
 
+    router.get("/", function (req, res, next) {
+      res.status(200).send({
+        api,
+      });
+    });
+  }, 10000);
+} catch {
   router.get("/", function (req, res, next) {
     res.status(200).send({
-      api,
+      message: "error",
     });
   });
-}, 10000);
+}
 
 module.exports = router;
