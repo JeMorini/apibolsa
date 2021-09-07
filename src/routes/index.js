2dd556d38c0d6ad98b75e3efd70357dc1af60fef
José Eduardo Morini Castro <Jemorini13@gmail.com>
2021-09-06 23:20:04 -0300

    deploy corrections

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,10 @@ let api = [];
 
 setInterval(async () => {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    });
     const page = await browser.newPage();
     await page.goto("https://br.investing.com/equities/");
     const data = await page.$$eval("table tr td", (tds) =>
